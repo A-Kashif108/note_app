20ae94b6d7bbc9607c07789efcd1f801241427fe
NanoNish <nishkarsh1206@gmail.com>
2023-01-19 23:44:49 +0530

    chore: formatting changes

--- lib/utils/color_combination.dart
+++ lib/utils/color_combination.dart
@@ -1,22 +1,20 @@
-
 class ColorCombination {
-  static const List<Map<String,int>> colorCombinations = [
-    {"primary":0xffF4DF4E,"secondary":0xff949398},
-    {"primary":0xff949398,"secondary":0xffF4DF4E},
-    {"primary":0xff5F4B8B,"secondary":0xffE69A8D},
-    {"primary":0xffE69A8D,"secondary":0xff5F4B8B},
-    {"primary":0xff42EADD,"secondary":0xffCDB599},
-    {"primary":0xffCDB599,"secondary":0xff42EADD},
-    {"primary":0xff00203F,"secondary":0xffADEFD1},
-    {"primary":0xffADEFD1,"secondary":0xff00203F},
-    {"primary":0xff606060,"secondary":0xffD6ED17},
-    {"primary":0xffD6ED17,"secondary":0xff606060},
-    {"primary":0xff97BC62,"secondary":0xff2C5F2D},
-    {"primary":0xff2C5F2D,"secondary":0xff97BC62},
-    {"primary":0xff00539C,"secondary":0xffEEA47F},
-    {"primary":0xffEEA47F,"secondary":0xff00539C},
-    {"primary":0xff0063B2,"secondary":0xff9CC3D5},
-    {"primary":0xff9CC3D5,"secondary":0xff0063B2},
+  static const List<Map<String, int>> colorCombinations = [
+    {"primary": 0xffF4DF4E, "secondary": 0xff949398},
+    {"primary": 0xff949398, "secondary": 0xffF4DF4E},
+    {"primary": 0xff5F4B8B, "secondary": 0xffE69A8D},
+    {"primary": 0xffE69A8D, "secondary": 0xff5F4B8B},
+    {"primary": 0xff42EADD, "secondary": 0xffCDB599},
+    {"primary": 0xffCDB599, "secondary": 0xff42EADD},
+    {"primary": 0xff00203F, "secondary": 0xffADEFD1},
+    {"primary": 0xffADEFD1, "secondary": 0xff00203F},
+    {"primary": 0xff606060, "secondary": 0xffD6ED17},
+    {"primary": 0xffD6ED17, "secondary": 0xff606060},
+    {"primary": 0xff97BC62, "secondary": 0xff2C5F2D},
+    {"primary": 0xff2C5F2D, "secondary": 0xff97BC62},
+    {"primary": 0xff00539C, "secondary": 0xffEEA47F},
+    {"primary": 0xffEEA47F, "secondary": 0xff00539C},
+    {"primary": 0xff0063B2, "secondary": 0xff9CC3D5},
+    {"primary": 0xff9CC3D5, "secondary": 0xff0063B2},
   ];
 }
-
